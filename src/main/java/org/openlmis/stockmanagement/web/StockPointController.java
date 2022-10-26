443d18c8882a9639ba36742a0a4d8c6ff6d5dc70
Leke <leke@techieplanetltd.com>
2022-10-26 12:13:46 +0100

    stock point end to end

---
+++ src/main/java/org/openlmis/stockmanagement/web/StockPointController.java
@@ -0,0 +1,40 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package main.java.org.openlmis.stockmanagement.web;
+
+import java.util.List;
+import org.openlmis.stockmanagement.domain.stockpoint.StockPoint;
+import org.openlmis.stockmanagement.service.abstracts.StockPointService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.profiler.Profiler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v3/stockCardSummaries")
+public class StockPointController {
+    @Autowired
+    StockPointService stockPointService;
+
+    public List<StockPoint> getStockPoints() {
+        return stockPointService.findStockPoints();
+    }
+}
