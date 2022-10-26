74543bd3409053dd567c9298e72f029a706a3caa
Leke <leke@techieplanetltd.com>
2022-10-26 20:38:19 +0100

    made StockPoint entity immutable

--- src/main/java/org/openlmis/stockmanagement/domain/stockpoint/StockPoint.java
+++ src/main/java/org/openlmis/stockmanagement/domain/stockpoint/StockPoint.java
@@ -22,7 +22,6 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Immutable;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -33,6 +32,7 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Immutable;
 
 
 @Entity
