3ed6a80154ba9ba96f826f3ce26627eb428ceea3
Asquarep <asquarep@yahoo.com>
2022-11-07 15:30:41 +0100

    corrected ID in issueSummary

--- src/main/java/org/openlmis/stockmanagement/web/StockPointsController.java
+++ src/main/java/org/openlmis/stockmanagement/web/StockPointsController.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.profiler.Profiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
