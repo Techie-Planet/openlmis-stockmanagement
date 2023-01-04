c83e3fdc7de981176e0d6dccc2b8e318635f395e
Asquarep <asquarep@yahoo.com>
2023-01-04 02:18:35 +0100

    physical inventory notifications

--- src/main/java/org/openlmis/stockmanagement/service/StockEventNotificationProcessor.java
+++ src/main/java/org/openlmis/stockmanagement/service/StockEventNotificationProcessor.java
@@ -99,52 +99,52 @@ public class StockEventNotificationProcessor {
     XLOGGER.exit();
   }
 
-  private void callIssueNotifications(StockEventDto event, Map.Entry<FacilityDto,
-      List<StockEventLineItemDto>> eventLine, UUID rightId) {
-    XLOGGER.entry(event, eventLine);
-    Profiler profiler = new Profiler("CALL_ISSUE_NOTIFICATION_FOR_LINE_ITEM");
-    profiler.setLogger(XLOGGER);
-
-    profiler.start("COPY_STOCK_CARD");
-    OrderableLotIdentity identity = OrderableLotIdentity.identityOf(eventLine.getValue().get(0));
-    StockCard stockCard = event.getContext().findCard(identity);
-
-    issueNotifier.notifyStockEditors(stockCard, rightId,
-        eventLine.getValue().size(), event.getFacilityId());
-
-    profiler.stop().log();
-    XLOGGER.exit();
-  }
-  // TODO this is a notifier class... create a notification method for issue here
-
-  /**
-   * Notify users of an issue event.
-   * zero. If so, send a notification to all of that stock card's editors.
-   *
-   * @param eventDto the stock event to process
-   * @param stockEventLineItems the stock event line items
-   */
-  public void notifyIssue(StockEventDto eventDto, Map<StockEventLineItemDto,
-      FacilityDto> stockEventLineItems) {
-    RightDto right = rightReferenceDataService.findRight(STOCK_ADJUST);
-
-    Map<FacilityDto, List<StockEventLineItemDto>> numberInEachFacility = new HashMap<>();
-    // this block checks if different stock event line items are going to the same facility
-    // and collects them into a list
-    for (Map.Entry<StockEventLineItemDto, FacilityDto> eachEntry: stockEventLineItems.entrySet()) {
-      if (numberInEachFacility.get(eachEntry.getValue()) == null) {
-        List<StockEventLineItemDto> list = new ArrayList<>();
-        list.add(eachEntry.getKey());
-        numberInEachFacility.put(eachEntry.getValue(), list);
-      } else {
-        List<StockEventLineItemDto> list = numberInEachFacility.get(eachEntry.getValue());
-        list.add(eachEntry.getKey());
-        numberInEachFacility.put(eachEntry.getValue(), list);
-      }
-    }
-    for (Map.Entry<FacilityDto, List<StockEventLineItemDto>> entry
-            : numberInEachFacility.entrySet()) {
-      callIssueNotifications(eventDto, entry, right.getId());
-    }
-  }
+  //  private void callIssueNotifications(StockEventDto event, Map.Entry<FacilityDto,
+  //      List<StockEventLineItemDto>> eventLine, UUID rightId) {
+  //    XLOGGER.entry(event, eventLine);
+  //    Profiler profiler = new Profiler("CALL_ISSUE_NOTIFICATION_FOR_LINE_ITEM");
+  //    profiler.setLogger(XLOGGER);
+  //
+  //    profiler.start("COPY_STOCK_CARD");
+  //    OrderableLotIdentity identity = OrderableLotIdentity.identityOf(eventLine.getValue().get(0));
+  //    StockCard stockCard = event.getContext().findCard(identity);
+  //
+  //    issueNotifier.notifyStockEditors(stockCard, rightId,
+  //        eventLine.getValue().size(), event.getFacilityId());
+  //
+  //    profiler.stop().log();
+  //    XLOGGER.exit();
+  //  }
+  //  // TODO this is a notifier class... create a notification method for issue here
+  //
+  //  /**
+  //   * Notify users of an issue event.
+  //   * zero. If so, send a notification to all of that stock card's editors.
+  //   *
+  //   * @param eventDto the stock event to process
+  //   * @param stockEventLineItems the stock event line items
+  //   */
+  //  public void notifyIssue(StockEventDto eventDto, Map<StockEventLineItemDto,
+  //      FacilityDto> stockEventLineItems) {
+  //    RightDto right = rightReferenceDataService.findRight(STOCK_ADJUST);
+  //
+  //    Map<FacilityDto, List<StockEventLineItemDto>> numberInEachFacility = new HashMap<>();
+  //    // this block checks if different stock event line items are going to the same facility
+  //    // and collects them into a list
+  //    for (Map.Entry<StockEventLineItemDto, FacilityDto> eachEntry: stockEventLineItems.entrySet()) {
+  //      if (numberInEachFacility.get(eachEntry.getValue()) == null) {
+  //        List<StockEventLineItemDto> list = new ArrayList<>();
+  //        list.add(eachEntry.getKey());
+  //        numberInEachFacility.put(eachEntry.getValue(), list);
+  //      } else {
+  //        List<StockEventLineItemDto> list = numberInEachFacility.get(eachEntry.getValue());
+  //        list.add(eachEntry.getKey());
+  //        numberInEachFacility.put(eachEntry.getValue(), list);
+  //      }
+  //    }
+  //    for (Map.Entry<FacilityDto, List<StockEventLineItemDto>> entry
+  //            : numberInEachFacility.entrySet()) {
+  //      callIssueNotifications(eventDto, entry, right.getId());
+  //    }
+  //  }
 }
