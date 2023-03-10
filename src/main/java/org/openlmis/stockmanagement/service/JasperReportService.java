7c791461438f9d260d4119d35d2fc23f1edf6080
Asquarep <asquarep@yahoo.com>
2023-03-10 08:55:26 +0100

    testing now

--- src/main/java/org/openlmis/stockmanagement/service/JasperReportService.java
+++ src/main/java/org/openlmis/stockmanagement/service/JasperReportService.java
@@ -29,6 +29,7 @@ import java.sql.Connection;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
