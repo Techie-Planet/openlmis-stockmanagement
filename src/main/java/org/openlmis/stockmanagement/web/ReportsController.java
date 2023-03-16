408f24e13eb4c1cf7119d1ee969577fd1d63c17d
Asquarep <asquarep@yahoo.com>
2023-03-16 12:49:50 +0100

    sending back base64

--- src/main/java/org/openlmis/stockmanagement/web/ReportsController.java
+++ src/main/java/org/openlmis/stockmanagement/web/ReportsController.java
@@ -132,7 +132,7 @@ public class ReportsController {
 
     return ResponseEntity
             .ok()
-            // .contentType(MediaType.APPLICATION_PDF)
+             .contentType(MediaType.APPLICATION_PDF)
             .header("Content-Disposition",
                     "inline; filename=stock_adjustment_summary" + ".pdf")
             .body(reportToBase64);
