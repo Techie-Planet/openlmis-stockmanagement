36081c3f6d43227fe9751b33b04c67ce044c3a03
Asquarep <asquarep@yahoo.com>
2023-03-21 14:37:28 +0100

    source destination fix

--- src/main/java/org/openlmis/stockmanagement/web/ReportsController.java
+++ src/main/java/org/openlmis/stockmanagement/web/ReportsController.java
@@ -132,7 +132,6 @@ public class ReportsController {
 
     return ResponseEntity
             .ok()
-             .contentType(MediaType.APPLICATION_PDF)
             .header("Content-Disposition",
                     "inline; filename=stock_adjustment_summary" + ".pdf")
             .body(reportToBase64);
