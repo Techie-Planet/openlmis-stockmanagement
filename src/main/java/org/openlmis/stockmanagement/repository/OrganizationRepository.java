2462621f77c4dc9ef57012091730e8a14487984c
Asquarep <asquarep@yahoo.com>
2023-03-30 10:12:54 +0100

    added name param for filter function

--- src/main/java/org/openlmis/stockmanagement/repository/OrganizationRepository.java
+++ src/main/java/org/openlmis/stockmanagement/repository/OrganizationRepository.java
@@ -20,6 +20,7 @@ import java.util.UUID;
 import org.openlmis.stockmanagement.domain.sourcedestination.Organization;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
