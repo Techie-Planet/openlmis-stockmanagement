d637ebc58b232e88707aa4453a228ed73c90e165
Asquarep <asquarep@yahoo.com>
2023-05-10 10:16:24 +0100

    updating tests

--- src/integration-test/java/org/openlmis/stockmanagement/web/ValidSourceDestinationControllerIntegrationTest.java
+++ src/integration-test/java/org/openlmis/stockmanagement/web/ValidSourceDestinationControllerIntegrationTest.java
@@ -17,7 +17,7 @@ package org.openlmis.stockmanagement.web;
 
 import static java.util.Collections.singletonList;
 import static java.util.UUID.randomUUID;
-//import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
@@ -96,7 +96,7 @@ public class ValidSourceDestinationControllerIntegrationTest extends BaseWebTest
     when(validDestinationsCacheRepository.findFirst1ByProgramIdAndFacilityId(program, facility))
             .thenReturn(Optional.of(validDestinationsCache));
 
-    when(validDestinationsCacheRepository.findFirst1ByProgramIdAndFacilityId(program, facility))
+    when(validSourcesCacheRepository.findFirst1ByProgramIdAndFacilityId(program, facility))
             .thenReturn(Optional.of(validSourcesCache));
 
     when(validSourceService.findSources(program, facility, pageRequest))
