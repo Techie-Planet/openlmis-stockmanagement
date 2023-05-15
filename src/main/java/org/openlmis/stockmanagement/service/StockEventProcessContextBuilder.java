9e429b385a6c08e73571b4665b9d9f5963f49b5e
Asquarep <asquarep@yahoo.com>
2023-05-15 06:56:03 +0100

    assignment validator to use cache

--- src/main/java/org/openlmis/stockmanagement/service/StockEventProcessContextBuilder.java
+++ src/main/java/org/openlmis/stockmanagement/service/StockEventProcessContextBuilder.java
@@ -222,7 +222,7 @@ public class StockEventProcessContextBuilder {
     profiler.start("CREATE_LAZY_SOURCES");
     Supplier<List<ValidSourceAssignment>> sourcesSupplier = () ->
             validSourceService.getValidSourceAssignmentsList(
-                    eventDto.getProgramId(), context.context.getFacility().getId(), profiler);
+                    eventDto.getProgramId(), context.getFacility().getId(), profiler);
         //     validSourceAssignmentRepository
         // .findByProgramIdAndFacilityTypeId(
         //         eventDto.getProgramId(), context.getFacilityTypeId(), Pageable.unpaged());
